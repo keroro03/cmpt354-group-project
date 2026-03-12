5b1b55e2ba348effaa40fff3c1d8050f6b0ff650
James Hoang <qmh1@sfu.ca>
2026-03-11 23:11:02 -0700

    implemented db tables

--- db/migrations/01_library_schema.up.sql
+++ db/migrations/01_library_schema.up.sql
@@ -1,18 +1,49 @@
 -- branch
 CREATE TABLE IF NOT EXISTS branch (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- branch id
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- branch_id
   branch_name TEXT NOT NULL,
   location TEXT NOT NULL
 );
 
 -- member
-CREATE TABLE IF NOT EXISTS library_member (
+CREATE TABLE IF NOT EXISTS member (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- member_id
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
   email TEXT NOT NULL,
   phone_number TEXT,
   address TEXT
+);
+
+-- Member - Branch (handle) many-to-many
+CREATE TABLE IF NOT EXISTS member_branch (
+  member_id UUID NOT NULL REFERENCES member(id) ON DELETE RESTRICT,
+  branch_id UUID NOT NULL REFERENCES branch(id) ON DELETE RESTRICT,
+  PRIMARY KEY (member_id, branch_id)
+);
+
+-- staff
+CREATE TABLE staff (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- staff_id
+  branch_id UUID NOT NULL REFERENCES branch(id) ON DELETE RESTRICT,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL
+);
+
+-- inherites from staff
+CREATE TABLE librarian (
+  staff_id UUID PRIMARY KEY REFERENCES staff(id) ON DELETE RESTRICT,
+  certification TEXT
+);
+
+CREATE TABLE assistant_manager (
+  staff_id UUID PRIMARY KEY REFERENCES staff(id) ON DELETE RESTRICT
+);
+
+CREATE TABLE manager (
+  staff_id UUID PRIMARY KEY REFERENCES staff(id) ON DELETE RESTRICT,
+  office_id TEXT
 );
 
 -- author
@@ -31,13 +62,30 @@ CREATE TABLE IF NOT EXISTS book (
   isbn TEXT,
   publish_year INT,
   publisher TEXT,
-  genre TEXT,
+  genre TEXT
+);
+
+-- book author own many-to-many
+CREATE TABLE IF NOT EXISTS book_author (
+  book_id UUID NOT NULL REFERENCES book(id) ON DELETE RESTRICT,
+  author_id UUID NOT NULL REFERENCES author(id) ON DELETE RESTRICT,
+  PRIMARY KEY (book_id, author_id)
 );
 
 -- book_copies
 CREATE TABLE IF NOT EXISTS book_copies (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- copied_book_id
-  book_id UUID NOT NULL REFERENCES books(id) ON DELETE RESTRICT,
-  branch_id UUID NOT NULL REFERENCES branches(id) ON DELETE RESTRICT,
-  status copy_status NOT NULL DEFAULT 'AVAILABLE',
+  book_id UUID NOT NULL REFERENCES book(id) ON DELETE RESTRICT,
+  branch_id UUID NOT NULL REFERENCES branch(id) ON DELETE RESTRICT,
+  status copy_status NOT NULL DEFAULT 'AVAILABLE'
+);
+
+-- borrow
+CREATE TABLE IF NOT EXISTS borrow (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(), -- borrow_id
+  member_id UUID NOT NULL REFERENCES member(id) ON DELETE RESTRICT,
+  book_copy_id UUID NOT NULL REFERENCES book_copies(id) ON DELETE RESTRICT,
+  loan_date DATE NOT NULL DEFAULT CURRENT_DATE,
+  due_date DATE NOT NULL,
+  return_date DATE
 );
